77b5a30cbcb3a4f4e935e9945ea1657478568b39
TangHsiu-Pei <tanghsiu-pei@TangHsiu-PeideiMac.local>
2016-07-21 11:30:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tanghsiu_pei/javacoding/utilities/MyTextUtil.java
@@ -0,0 +1,11 @@
+package com.example.tanghsiu_pei.javacoding.utilities;
+
+/**
+ * Created by tanghsiu-pei on 2016/7/21.
+ */
+public class MyTextUtil {
+
+    public String getValue() {
+        return " Hello From the utility class";
+    }
+}
